8df9a08f26230902e98f9f39867811ab26e0dc51
JennicaStiehl <jennica.stiehl@gmail.com>
2019-04-25 09:05:11 -0600

    adds hand-rolled routes and all tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: [:index, :show, :new, :create, :destroy] do
-    resources :reviews, only: [:new, :create, :destroy]
-  end
+  # http://guides.rubyonrails.org/routing.html
 
-  # resources :books, only: [:new], params: {book: @book}
-  resources :authors, only: [:show, :destroy]
+  get '/books/new(.:format)', to: 'books#new', as: 'new_book'
+  get '/books', to: 'books#index', as: 'books'
+  get '/books/:id', to: 'books#show', as: 'book'
+  post '/books', to: 'books#create'
+  delete '/books/:id', to: 'books#destroy'
+
+  get '/books/:book_id/reviews/new', to: 'reviews#new', as: 'new_book_review'
+  post '/books/:book_id/reviews', to: 'reviews#create', as: 'book_reviews'
+  delete '/books/:book_id/reviews/:id', to: 'reviews#destroy', as: 'book_review'
+
+  get '/authors/:id', to: 'authors#show'
+  delete '/authors/:id', to: 'authors#destroy', as: 'author'
 
   get '/', to: 'welcome#index', as: 'welcome'
 
   get '/user/:user', to: 'reviews#user_show', as: 'user'
-  # get '/user/:user', to: 'reviews#user_show', as: 'user_asc'#, defaults: { user: :user }
-  # get '/user/:user', to: 'reviews#user_show', as: 'user_desc'
 end
